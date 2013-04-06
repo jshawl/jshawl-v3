8798de5c7404e934c1386e60d10a072ff33ba9e6
Jesse Shawl <jesse@jshawl.com>
2013-04-06 11:26:20 -0400

    new post!

--- public/docs/js/main.js
+++ public/docs/js/main.js
@@ -1,11 +1,29 @@
 $('[data-lang]').each(function(){
-	// get the language
-	console.log($(this));
-	var language = $(this).attr('data-lang');
-	console.log(language);
-	// add the data-lang as a class to the next sibling
-	$(this).parent().next().addClass(language);
-	//$(this).closest('pre').css({'border':'5px solid red'});
+
+    // get the language
+    var language = $(this).attr('data-lang');
+
+    // add the data-lang as a class to the next sibling
+    // this is pretty hack-y at the moment. The way I've been 
+    // writing markdown generates a DOM node that looks like this:
+    // <p>
+    //   Some text explaining the next code block
+    //   <span data-lang='lang-js'></span>
+    // </p>
+    // <pre class='prettify'>
+    //   <code>
+    //      // code!
+    //   </code>
+    // </pre>
+    //
+    // So I know (accurately guess) that the next code block can be referenced as:
+    // The next sibling of the parent of the span with the class [data-lang]
+    //
+    //  It might make more sense to look at a markdown example: 
+    //  https://github.com/jshawl/jshawl-v3/blob/master/content/2.thoughts/2.dealing-with-rate-limited-apis/single.txt#L54-59
+
+    $(this).parent().next().addClass(language);
+
 });
 
 $('pre').addClass('prettyprint');
